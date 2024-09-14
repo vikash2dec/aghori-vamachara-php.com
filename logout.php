02b653182bccd962c047275dfd9da12d171886d4
vikash2dec <116957492+vikash2dec@users.noreply.github.com>
2024-09-15 05:24:08 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+if(session_unset()){
+   header("location:index.php");
+}else{
+    
+}
+?>
---
+++ new_account.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Aghori Vamachara GYM / Login</title>
+</head>
+<body>
+    <div class="login-1">
+        <div class="login-2">
+            <div class="login-3">
+                <img src="">
+            </div>
+            <div class="login-4">
+                <h3>Aghori Vamachara <span>GYM</span></h3>                
+            </div>
+            <div class="login-5">
+                <form action="">
+                    <div class="login-6">
+                        <input type="text" name="phone" placeholder="phonenumber or email">
+                    </div>
+                    <button type="submit">Send Code</button>
+                </form>
+            </div>
+            <div class="login-8">
+                <h3>You have already account <span><a href="login.php">Login</a></span></h3>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        
+    </script>
+</body>
+</html>
